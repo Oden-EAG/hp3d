f0c30f3eb8350edbf6a1c4d472187415934def0e
Stefan Henneking <stefan.henneking@gmail.com>
2019-08-05 19:44:34 -0500

    OMP parallelize close_mesh and refresh routine.

--- trunk/src/meshmod/close_mesh.F90
+++ trunk/src/meshmod/close_mesh.F90
@@ -1,158 +1,190 @@
-subroutine close
-      implicit none
-      integer :: iprint
-!  ...ensure backward compatibility
-      iprint=0
-      if (iprint /= 0)  write(*,*) 'Mesh closing begin' 
-      call close_mesh
-      if (iprint /= 0)  write(*,*) 'Mesh closing end'
-endsubroutine close
+subroutine close()
+   implicit none
+   integer :: iprint
+!..ensure backward compatibility
+   iprint=0
+   if (iprint /= 0)  write(*,*) 'Mesh closing begin'
+   call close_mesh
+   if (iprint /= 0)  write(*,*) 'Mesh closing end'
+end subroutine close
 !
-!
+!-----------------------------------------------------------------------------
 !
 !> Purpose : enforce one-irregular mesh
 subroutine close_mesh()
-  use error
-  use refinements
-  use data_structure3D
-  implicit none
-  ! ** Locals
-  character(len=4)       :: type
-7001          format('close_mesh: i= ',i6,' mdle= ', i6,' ', a4, &
-                   ' ref_kind = ',i5)
-  integer, allocatable   :: list(:,:)
-  integer, dimension(27) :: nodesl,norientl
-  integer, dimension(6)  :: kreff
-  integer, dimension(12) :: krefe
-  integer :: iprint, istat, i, j, ic, mdle, nod, kref, nre, nrf 
-  logical :: nflag
-  !-------------------------------------------------------
-  iprint=0
-  !
-  ! allocate list
-  allocate(list(2,NRELES), stat=istat)
-  if (istat.ne.SUCCESS) then
-    call logic_error(ERR_ALLOC_FAILURE,__FILE__,__LINE__)
-  endif
-  !
-  !
-  !-------------------------------------------------------
-  do 
-     !---------------------------------------------------------
-     ! Step 0 : activate nodes
-     !---------------------------------------------------------
-     call refresh
-     
-     !---------------------------------------------------------
-     ! Step 1 : check constrained nodes
-     !---------------------------------------------------------
-     if (iprint.eq.2) then
-        write(*,*) 'close_mesh: CHECK CONSTRAINING NODES'
-     endif
-
-     call reset_visit
-     mdle=0
-     do i=1,NRELES
-        call nelcon(mdle, mdle)
-        call get_connect_info(mdle, nodesl,norientl)
-        call flag_constr_parents(mdle)
-     enddo
-
-     !---------------------------------------------------------
-     ! Step 2 : pick the middle nodes to be refined
-     !---------------------------------------------------------
-     if (iprint.eq.2) then
-        write(*,*) 'close_mesh: RESOLVE THE NODES MORE THAN ONE IRREGULARITY'
-     endif
-
-     mdle=0; ic=0
-     do i=1,NRELES
-
-        call nelcon(mdle, mdle)
-        call elem_nodes(mdle, nodesl,norientl)
-
-        type  = NODES(mdle)%type
-        nflag = .FALSE.
+   use error
+   use refinements
+   use data_structure3D
+   use mpi_param  , only: ROOT,RANK
+   use MPI        , only: MPI_COMM_WORLD,MPI_SUM,MPI_COMM_WORLD,MPI_REAL8
+!
+   implicit none
+!
+   character(4)           :: type
+   integer                :: list(2,NRELES)
+   integer                :: mdlel(NRELES)
+   integer, dimension(27) :: nodesl,norientl
+   integer, dimension(6)  :: kreff
+   integer, dimension(12) :: krefe
+   integer :: iprint, istat, i, j, ic, mdle, nod, kref, nre, nrf
+   logical :: nflag
+   real(8) :: MPI_Wtime,start_time,end_time
+   integer :: ierr
+!
+#if DEBUG_MODE
+   integer :: iprint
+   iprint = 0
+#endif
+!
+!-----------------------------------------------------------------------------
+!
+   do
+!---------------------------------------------------------
+!     Step 0 : activate nodes
+!---------------------------------------------------------
+      call MPI_BARRIER (MPI_COMM_WORLD, ierr); start_time = MPI_Wtime()
+      call refresh
+      call MPI_BARRIER (MPI_COMM_WORLD, ierr); end_time = MPI_Wtime()
+      if (RANK .eq. ROOT) write(*,2020) end_time-start_time
+ 2020 format(' refresh    : ',f12.5,'  seconds')
+!
+!---------------------------------------------------------
+!     Step 1 : check constrained nodes
+!---------------------------------------------------------
+#if DEBUG_MODE
+      if (iprint.eq.2) then
+         write(*,*) 'close_mesh: CHECK CONSTRAINING NODES'
+      endif
+#endif
+!
+      list(1:2,1:NRELES) = 0; ic = 0
+!
+      mdle=0
+      do i=1,NRELES
+         call nelcon(mdle, mdle)
+         mdlel(i) = mdle
+      enddo
+!
+!$OMP PARALLEL
+!$OMP DO
+      do i=1,NRNODS
+        NODES(i)%visit = 0
+      enddo
+!$OMP END DO
+!$OMP DO PRIVATE(mdle,nodesl,norientl)
+      do i=1,NRELES
+         mdle = mdlel(i)
+         call get_connect_info(mdle, nodesl,norientl)
+         call flag_constr_parents(mdle)
+      enddo
+!$OMP END DO
+!
+#if DEBUG_MODE
+!$OMP SINGLE
+      if (iprint.eq.2) then
+         write(*,*) 'close_mesh: RESOLVE THE NODES MORE THAN ONE IRREGULARITY'
+      endif
+!$OMP END SINGLE
+#endif
+!
+!---------------------------------------------------------
+!     Step 2 : pick the middle nodes to be refined
+!---------------------------------------------------------
+!
+!$OMP DO                                     &
+!$OMP PRIVATE(mdle,nodesl,norientl,nod,type, &
+!$OMP         nflag,krefe,kreff,kref,j)      &
+!$OMP REDUCTION (+:ic)
+      do i=1,NRELES
+         mdle=mdlel(i)
+         call elem_nodes(mdle, nodesl,norientl)
+!
+         type  = NODES(mdle)%type
+         nflag = .FALSE.
         
-        ! check edges
-        !~~~~~~~~~~~~
-        krefe = 0
-        do j=1, nedge(type)
-           nod = nodesl(nvert(type)+j)
-           if (NODES(nod)%visit.eq.1) then
-              krefe(j)=1
-              nflag=.TRUE.
-           endif
-        enddo
+!        check edges
+!        ~~~~~~~~~~~~
+         krefe = 0
+         do j=1, nedge(type)
+            nod = nodesl(nvert(type)+j)
+            if (NODES(nod)%visit.eq.1) then
+               krefe(j)=1
+               nflag=.TRUE.
+            endif
+         enddo
 
-        ! check faces
-        !~~~~~~~~~~~~
-        kreff = 0
-        do j=1, nface(type)
-           nod = nodesl(nvert(type)+nedge(type)+j)
-           if (NODES(nod)%visit.eq.1) then
-              call get_isoref(nod, kreff(j))
-              nflag=.TRUE.
-           endif
-        enddo
-
-        ! find out more than one-irregular
-        !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        if (nflag) then
-           if (iprint.eq.1) then
-              nre = nedge(NODES(mdle)%type)
-              nrf = nface(NODES(mdle)%type)
-              write(*,*) 'close_mesh: mdle = ', mdle
-              write(*,7003) krefe(1:nre)
-7003          format('krefe = ',12i2)
-              write(*,7004) kreff(1:nrf)
-7004          format('kreff = ',6i2)
-              call pause
-           endif
-
-           ! add counter
-           ic=ic+1
-
-           ! find out proper refinement flag
-           if ( is_iso_only() ) then
-              call get_isoref(mdle, kref)
-           else
-              call find_element_closing_ref(type,kreff,krefe, kref)
-           endif
-           list(1,ic) = mdle
-           list(2,ic) = kref
-        endif
-     enddo
-
-     ! loop out condition
-     if (iprint.eq.1) then
-        write(*,*) 'close_mesh: number of elements to refine ', ic
-     endif
-     if (ic.eq.0) then 
-        exit
-     end if
-
-     !---------------------------------------------------------
-     ! Step : refine from the list
-     !---------------------------------------------------------
-     do i=1,ic
-        mdle = list(1,i)
-        kref = list(2,i)
-        if ( is_leaf( mdle ) ) then
-           if (iprint.eq.2) then
-              write(*,7001) i, mdle, NODES(mdle)%type, kref 
-           endif
-           call refine( mdle, kref )
-        endif
-     enddo
-  enddo
-  !
-  !
-  !
-  deallocate(list, stat=istat) 
-  if (istat.ne.SUCCESS) then
-    call logic_error(ERR_ALLOC_FAILURE,__FILE__,__LINE__)
-  endif
+!        check faces
+!        ~~~~~~~~~~~~
+         kreff = 0
+         do j=1, nface(type)
+            nod = nodesl(nvert(type)+nedge(type)+j)
+            if (NODES(nod)%visit.eq.1) then
+               call get_isoref(nod, kreff(j))
+               nflag=.TRUE.
+            endif
+         enddo
 !
+!        find out more than one-irregular
+!        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         if (nflag) then
 !
-endsubroutine close_mesh
+#if DEBUG_MODE
+!$OMP CRITICAL
+            if (iprint.eq.1) then
+               nre = nedge(NODES(mdle)%type)
+               nrf = nface(NODES(mdle)%type)
+               write(*,*) 'close_mesh: mdle = ', mdle
+               write(*,7003) krefe(1:nre)
+7003           format('krefe = ',12i2)
+               write(*,7004) kreff(1:nrf)
+7004           format('kreff = ',6i2)
+               call pause
+            endif
+!$OMP END CRITICAL
+#endif
+!
+!           increment counter
+            ic=ic+1
+!
+!           find out proper refinement flag
+            if ( is_iso_only() ) then
+               call get_isoref(mdle, kref)
+            else
+               call find_element_closing_ref(type,kreff,krefe, kref)
+            endif
+            list(1,i) = mdle
+            list(2,i) = kref
+         endif
+      enddo
+!$OMP END DO
+!$OMP END PARALLEL
+!
+#if DEBUG_MODE
+      if (iprint.eq.1) then
+         write(*,*) 'close_mesh: number of elements to refine ', ic
+      endif
+#endif
+!
+!     loop exit condition
+      if (ic.eq.0) exit
+!
+!---------------------------------------------------------
+!     Step : refine from the list
+!---------------------------------------------------------
+      do i=1,NRELES
+         if (list(1,i) .eq. 0) cycle
+         mdle = list(1,i)
+         kref = list(2,i)
+         if ( is_leaf( mdle ) ) then
+#if DEBUG_MODE
+            if (iprint.eq.2) then
+               write(*,7001) i, mdle, NODES(mdle)%type, kref
+ 7001          format('close_mesh: i= ',i6,' mdle= ', i6,' ', a4, ' ref_kind = ',i5)
+            endif
+#endif
+            call refine( mdle, kref )
+         endif
+      enddo
+   enddo
+!
+end subroutine close_mesh
--- trunk/src/meshmod/refresh.F90
+++ trunk/src/meshmod/refresh.F90
@@ -13,8 +13,9 @@ subroutine refresh
 !
    implicit none
 !
-   character(len=4) :: type
-   integer, dimension(27) :: nodesl,norientl
+   character(4) :: type
+   integer :: nodesl(27),norientl(27)
+   integer :: mdlel(NRELES)
    integer :: iprint,i,j,iel,nod,nfath,mdle,ibegin,iend, &
               nrsons,loc,subd
 !
@@ -22,20 +23,33 @@ subroutine refresh
 !
    iprint=0
 !
+#if DEBUG_MODE
    if (iprint.eq.1) then
       write(*,*) 'refresh: Begin'
    endif
+#endif
+!
+   mdle=0
+   do iel=1,NRELES
+      call nelcon(mdle, mdle)
+      mdlel(iel) = mdle
+   enddo
 !
 !..reset visitation flags for all nodes
-   call reset_visit
+!$OMP PARALLEL
+!$OMP DO
+      do i=1,NRNODS
+        NODES(i)%visit = 0
+      enddo
+!$OMP END DO
 !  
 !--------------------------------------------------------------------
 ! Step 1 : raise visitation flag for vertex, edge and face nodes of |
 !          all active elements                                      |
 !--------------------------------------------------------------------
-   mdle=0
+!$OMP DO PRIVATE(mdle,subd,nodesl,norientl,type,ibegin,iend,i)
    do iel=1,NRELES
-      call nelcon(mdle, mdle)
+      mdle = mdlel(iel)
       call get_subd(mdle, subd)
       call elem_nodes(mdle, nodesl,norientl)
       type=NODES(mdle)%type
@@ -51,13 +65,16 @@ subroutine refresh
          endif
       enddo
    enddo
-!  
+!$OMP END DO
+!
 !--------------------------------------------------------------------
 ! Step 2: activate all inactive edge and face nodes whose father    |
 !         node was not visited                                      |
 !--------------------------------------------------------------------
 !
 !..loop over all nodes
+!$OMP DO SCHEDULE(DYNAMIC)       &
+!$OMP PRIVATE(nfath,nrsons,loc)
    do nod=1,NRNODS
 !
 !  ...skip if a middle node
@@ -70,43 +87,65 @@ subroutine refresh
 !
 !  ...skip if active
       if (NODES(nod)%act.eq.1) cycle
-!     
+!
+#if DEBUG_MODE
+!$OMP CRITICAL
       if (iprint.eq.1) then
          write(*,7010) nod
  7010    format('refresh: INACTIVE MARKED NODE nod = ',i7)
       endif
+!$OMP END CRITICAL
+#endif
 !
       nfath=NODES(nod)%father
+!
+#if DEBUG_MODE
+!$OMP CRITICAL
       if (nfath.le.0) then
          write(*,*) 'refresh: INCONSISTENCY: nod = ',nod
          stop
       endif
+!$OMP END CRITICAL
+#endif
 !
 !  ...if father node has not been visited, activate the node
       if (NODES(nfath)%visit.eq.0) then
          call activate(nod)
+!
+#if DEBUG_MODE
+!$OMP CRITICAL
          if (iprint.eq.1) then
             write(*,7020) nod
  7020       format('refresh: ACTIVATED nod = ',i6)
          endif
+!$OMP END CRITICAL
+#endif
 !
 !     ...if this is the last son, deactivate the father
-         nrsons = ubound(NODES(nfath)%sons,1)
-         call locate(nod,NODES(nfath)%sons,nrsons, loc)
+!         nrsons = ubound(NODES(nfath)%sons,1)
+!         call locate(nod,NODES(nfath)%sons,nrsons, loc)
+         nrsons = NODES(nfath)%nr_sons
+         loc = nod - NODES(nfath)%first_son + 1
+!         if (loc<0 .or. loc>nrsons) call pause
          if (loc.eq.nrsons) then
             call deactivate(nfath)
+!
+#if DEBUG_MODE
+!$OMP CRITICAL
             if (iprint.eq.1) then
                write(*,7030) nfath
  7030          format('refresh: DEACTIVATED nfath, loc, nrsons = ',i6,2i3)
             endif
+!$OMP END CRITICAL
+#endif
+!
          endif
       endif
 !
 !..end of loop over nodes
    enddo
-!
-!..reset visitation flags
-   call reset_visit
+!$OMP END DO
+!$OMP END PARALLEL
 !
 !
 end subroutine refresh
