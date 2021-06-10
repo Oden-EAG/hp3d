bf6f1e8e2fc7699d3ec5fb5254f44907576f08cf
stefanhenneking <stefan.henneking@gmail.com>
2021-06-09 22:50:49 -0500

    style and debug flags.

--- trunk/problems/MPI_VECTOR_POISSON/GALERKIN/elem.F90
+++ trunk/problems/MPI_VECTOR_POISSON/GALERKIN/elem.F90
@@ -18,39 +18,39 @@
 !
 !----------------------------------------------------------------------
 !
-      subroutine elem(Mdle, Itest,Itrial)
+subroutine elem(Mdle, Itest,Itrial)
 !
-      use data_structure3D
-      use physics  , only: NR_PHYSA
-      use assembly , only: ALOC,BLOC
+   use data_structure3D
+   use physics  , only: NR_PHYSA
+   use assembly , only: ALOC,BLOC
 !
-      implicit none
+   implicit none
 !
-      integer,                    intent(in)  :: Mdle
-      integer,dimension(NR_PHYSA),intent(out) :: Itest,Itrial
+   integer,                    intent(in)  :: Mdle
+   integer,dimension(NR_PHYSA),intent(out) :: Itest,Itrial
 !
-      integer :: nn
+   integer :: nn
 !
 !----------------------------------------------------------------------
 !
-!  ...activate one physics variable (H1) for assembly
-      Itest(1) = 1; Itrial(1) = 1
+!..activate one physics variable (H1) for assembly
+   Itest(1) = 1; Itrial(1) = 1
 !
-!  ...determine leading dimension of ALOC(1,1)%array
-      nn = ubound(ALOC(1,1)%array,1)
+!..determine leading dimension of ALOC(1,1)%array
+   nn = ubound(ALOC(1,1)%array,1)
 !
-!  ...call element integration routine
-      call elem_vect_poisson(Mdle,nn, ALOC(1,1)%array,BLOC(1)%array)
+!..call element integration routine
+   call elem_vect_poisson(Mdle,nn, ALOC(1,1)%array,BLOC(1)%array)
 !
-      end subroutine elem
-!
-!-------------------------------------------------------------------------
-!
-!     routine name      - elem_poisson
+end subroutine elem
 !
 !-------------------------------------------------------------------------
 !
-!     latest revision:  - July 2019
+!     routine name      - elem_vect_poisson
+!
+!-------------------------------------------------------------------------
+!
+!     latest revision:  - June 2021
 !
 !     purpose:          - compute element stiffness and load
 !
@@ -65,214 +65,170 @@
 !
 !-------------------------------------------------------------------------
 !
-      subroutine elem_vect_poisson(Mdle,Nn, Zaloc,Zbloc)
+subroutine elem_vect_poisson(Mdle,Nn, Zaloc,Zbloc)
 !
-      use control , only: INTEGRATION
-      use data_structure3D
-      use element_data
-      use parameters
-      use mpi_param, only: RANK
+   use control , only: INTEGRATION
+   use data_structure3D
+   use element_data
+   use parameters
+   use mpi_param, only: RANK
 !
-      implicit none
+   implicit none
 !
-      integer, intent(in)  :: Mdle
-      integer, intent(in)  :: Nn
-      real(8), intent(out) :: Zaloc(Nn,Nn), Zbloc(Nn)
+   integer, intent(in)  :: Mdle
+   integer, intent(in)  :: Nn
+   real(8), intent(out) :: Zaloc(Nn,Nn), Zbloc(Nn)
 !
-!  ...aux variables
-      real(8) :: rjac, fval, gval, wa, weight, q, p
-      integer :: iflag, nrv, nre, nrf
-      integer :: nrdofH,nrdofE,nrdofV,nrdofQ,nint,k1,k2,l,ivar,n1,n2
+!..aux variables
+   real(8) :: rjac, fval, gval, wa, weight, q, p
+   integer :: iflag, nrv, nre, nrf
+   integer :: nrdofH,nrdofE,nrdofV,nrdofQ,nint,k1,k2,l,ivar,n1,n2
 !
 !----------------------------------------------------------------------
 !
-      character(len=4) :: etype,ftype
+   character(len=4) :: etype,ftype
 !
-!  ...element order, orientation for edges and faces
-      integer :: norder(19), norient_edge(12), norient_face(6)
+!..element order, orientation for edges and faces
+   integer :: norder(19), norient_edge(12), norient_face(6)
 !
-!  ...face order
-      integer :: norderf(5)
+!..face order
+   integer :: norderf(5)
 !
-!  ...BC flags
-      integer :: ibc(6,NRINDEX)
+!..BC flags
+   integer :: ibc(6,NRINDEX)
 !
-!  ...geometry dof
-      real(8) :: xnod(3,MAXbrickH)
+!..geometry dof
+   real(8) :: xnod(3,MAXbrickH)
 !
 !..geometry
-! [
-!  xi     coordinates in reference element
-!  dxidt  d(xi) / dt
-!  x      coordinates in physical domain
-!  dxdxi  d(x) / dxi  Jacobian
-!  dxidx  d(xi) / dx  Inverse Jacobian
-!  dxdt   d(x) / dt
-!  rt     .
-!  rn     normal vec
-!  t      .
-! ]
-      real(8) :: xi(3), dxidt(3,2), x(3), dxdxi(3,3), dxidx(3,3), dxdt(3,2)
-      real(8) :: rt(3,2), rn(3), t(2),bjac
+   real(8) :: xi(3), dxidt(3,2), x(3), dxdxi(3,3), dxidx(3,3), dxdt(3,2)
+   real(8) :: rt(3,2), rn(3), t(2),bjac
 !
-!  ...H1 shape functions
-      real(8) :: shapH(MAXbrickH), gradH(3,MAXbrickH)
+!..H1 shape functions
+   real(8) :: shapH(MAXbrickH), gradH(3,MAXbrickH)
 !
-!  ...3D quadrature data
-!  [xiloc: integration points (local coordinates)]
-!  [waloc: integration weights per integration point]
-      real(8) :: xiloc(3,MAX_NINT3), waloc(MAX_NINT3)
+!..3D quadrature data
+   real(8) :: xiloc(3,MAX_NINT3), waloc(MAX_NINT3)
 !
-!  ...2D quadrature data
-      real(8) :: tloc(2,MAX_NINT2), wtloc(MAX_NINT2)
+!..2D quadrature data
+   real(8) :: tloc(2,MAX_NINT2), wtloc(MAX_NINT2)
 !
-!  ...workspace for trial and test variables
-      real(8) :: dq(3), u(3), dp(1:3), v(3), vec(3)
+!..workspace for trial and test variables
+   real(8) :: dq(3), u(3), dp(1:3), v(3), vec(3)
 !
-      integer :: iprint,ivar1,ivar2,ifc,nsign
+   integer :: ivar1,ivar2,ifc,nsign
+!
+#if DEBUG_FLAG
+   integer :: iprint = 0
+#endif
 !
 !----------------------------------------------------------------------
 !
-      iprint=0
-      Zaloc = ZERO; Zbloc = ZERO
+   Zaloc = ZERO; Zbloc = ZERO
 !
-!  ...element type
-      etype = NODES(Mdle)%type
-!  ...[number of vertices, edges, and faces of this element (type)]
-      nrv = nvert(etype)
-      nre = nedge(etype)
-      nrf = nface(etype)
+!..element type
+   etype = NODES(Mdle)%type
+!..[number of vertices, edges, and faces of this element (type)]
+   nrv = nvert(etype)
+   nre = nedge(etype)
+   nrf = nface(etype)
 !
-!write(*,2050) '[', RANK, '] FIND ORDER'; call pause
-!  ...determine order of approximation
-      call find_order(Mdle, norder)
-      if (iprint.eq.1) then
-        write(*,7010) norder(1:nre+nrf+1)
- 7010   format('elem_vect_poisson: norder = ',19i4)
-      endif
+!..determine order of approximation
+   !write(*,2050) '[', RANK, '] FIND ORDER'; call pause
+   call find_order(Mdle, norder)
 !
-!write(*,2050) '[', RANK, '] FIND ORIENT'; call pause
-!  ...determine edge and face orientations
-      call find_orient(Mdle, norient_edge,norient_face)
+#if DEBUG_FLAG
+   if (iprint.eq.1) then
+     write(*,7010) norder(1:nre+nrf+1)
+7010 format('elem_vect_poisson: norder = ',19i4)
+   endif
+#endif
 !
-!  ...determine BC flags
-      call find_bc(Mdle, ibc)
-      if (iprint.eq.1) then
-         do ivar=1,3
-            write(*,7020) ivar, ibc(1:6,ivar)
-    7020    format('elem_vect_poisson: BC flags for ivar = ',i1,2x,6i2)
-         enddo
+!..determine edge and face orientations
+   !write(*,2050) '[', RANK, '] FIND ORIENT'; call pause
+   call find_orient(Mdle, norient_edge,norient_face)
+!
+!..determine BC flags
+   call find_bc(Mdle, ibc)
+!
+#if DEBUG_FLAG
+   if (iprint.eq.1) then
+      do ivar=1,3
+         write(*,7020) ivar, ibc(1:6,ivar)
+ 7020    format('elem_vect_poisson: BC flags for ivar = ',i1,2x,6i2)
       enddo
+   endif
+#endif
 !
-!  ...determine nodes coordinates
-!write(*,2050) '[', RANK, '] FIND NODCOR'; call pause
-      call nodcor(Mdle, xnod)
+!..determine nodes coordinates
+   !write(*,2050) '[', RANK, '] FIND NODCOR'; call pause
+   call nodcor(Mdle, xnod)
 !
 !----------------------------------------------------------------------
 !     E L E M E N T    I N T E G R A L S                              |
 !----------------------------------------------------------------------
 !
-!  ...use the order to set the quadrature
-!  [
-!  in:
-!   Type      - element type
-!   Norder    - order of approximation
+!..use the order to set the quadrature
+   call set_3D_int(etype,norder,norient_face, nint,xiloc,waloc)
 !
-!  out:
-!   Nint      - number of integration points
-!   Xiloc     - integration points
-!   Waloc     - weights
-!  ]
-      call set_3D_int(etype,norder,norient_face, nint,xiloc,waloc)
+!..loop over integration points
+   do l=1,nint
 !
-!  ...loop over integration points
-      do l=1,nint
+!  ...coordinates and weight of this integration point
+      xi(1:3)=xiloc(1:3,l);
+      wa=waloc(l)
 !
-!  .....coordinates and weight of this integration point
-        xi(1:3)=xiloc(1:3,l);
-        wa=waloc(l)
+!  ...H1 shape functions (for geometry)
+      call shape3DH(etype,xi,norder,norient_edge,norient_face, nrdofH,shapH,gradH)
 !
-! H1 shape functions (for geometry)
-! [
-!     in:
-!       Type           - element type
-!       Xi             - master element coordinates
-!       Norder         - polynomial order for the nodes (H1 sense)
-!       Nedge_orient   - edge orientations
-!       Nface_orient   - face orientations
-!     out:
-!       NrdofH         - number of the element shape functions
-!       ShapH          - values of shape functions
-!       GradH          - values of derivatives of the shape functions
-!                        wrt to master element coordinates
+!  ...geometry map
+      call geom3D(Mdle,xi,xnod,shapH,gradH,nrdofH, x,dxdxi,dxidx,rjac,iflag)
 !
-! ]
-        call shape3DH(etype,xi,norder,norient_edge,norient_face, nrdofH,shapH,gradH)
+!  ...integration weight
+      weight = rjac*wa
 !
-! geometry map
-! [
-!     in:
-!             Mdle     - element middle node
-!             Xi       - master element coordinates of a point
-!             Xnod     - element geometry dof
-!             ShapH    - values of shape functions at a point
-!             GradH    - derivatives of the shape functions wrt
-!                        master coordinates at the point
-!             NrdofH   - number of element H1 dof
-!     out:
-!             X        - physical coordinates of the point
-!             Dxdxi    - Jacobian matrix
-!             Dxidx    - inverse Jacobian matrix
-!             Rjac     - jacobian (determinant of the Jacobian matrix)
-!             Iflag    = 0 OK
-!                        1 negative jacobian
-! ]
-        call geom3D(Mdle,xi,xnod,shapH,gradH,nrdofH, x,dxdxi,dxidx,rjac,iflag)
+!  ...get the RHS
+      call getf(Mdle, x,fval)
 !
-!  .....integration weight
-        weight = rjac*wa
+!  ...loop through H1 test functions
+      do k1=1,nrdofH
 !
-!  .....get the RHS
-        call getf(Mdle, x,fval)
+!     ...Piola transformation
+         q = shapH(k1)
+         dq(1:3) = gradH(1,k1)*dxidx(1,1:3) &
+                 + gradH(2,k1)*dxidx(2,1:3) &
+                 + gradH(3,k1)*dxidx(3,1:3)
 !
-!  .....loop through H1 test functions
-        do k1=1,nrdofH
-!
-!  .......Piola transformation
-          q = shapH(k1)
-          dq(1:3) = gradH(1,k1)*dxidx(1,1:3) &
-                  + gradH(2,k1)*dxidx(2,1:3) &
-                  + gradH(3,k1)*dxidx(3,1:3)
-!
-!  .......accumulate for the load vector
-          do ivar=1,3
+!     ...accumulate for the load vector
+         do ivar=1,3
             n1 = (k1-1)*3 + ivar
             Zbloc(n1) = Zbloc(n1) + q*fval*weight
-          enddo
+         enddo
 !
-!  ......second loop through H1 trial functions
+!     ...second loop through H1 trial functions
          do k2=1,nrdofH
-!  .........Piola transformation
+!        ...Piola transformation
             p = shapH(k2)
             dp(1:3) = gradH(1,k2)*dxidx(1,1:3) &
                     + gradH(2,k2)*dxidx(2,1:3) &
                     + gradH(3,k2)*dxidx(3,1:3)
 !
-!  .........accumulate for the stiffness matrix
+!        ...accumulate for the stiffness matrix
             do ivar=1,3
-              n1 = (k1-1)*3 + ivar
-              n2 = (k2-1)*3 + ivar
-              Zaloc(n1,n2) = Zaloc(n1,n2) + weight * ( &
-                             dq(1)*dp(1) + dq(2)*dp(2) + dq(3)*dp(3))
+               n1 = (k1-1)*3 + ivar
+               n2 = (k2-1)*3 + ivar
+               Zaloc(n1,n2) = Zaloc(n1,n2) + weight * ( &
+                              dq(1)*dp(1) + dq(2)*dp(2) + dq(3)*dp(3))
             enddo
 !
-!  .......end of loop through H1 trial functions
-          enddo
-!  .....end of loop through H1 test functions
-        enddo
-!  ...end of loop through integration points
+!     ...end of loop through H1 trial functions
+         enddo
+!  ...end of loop through H1 test functions
       enddo
+!..end of loop through integration points
+   enddo
 !
+#if DEBUG_FLAG
       if (iprint.eq.1) then
   100   write(*,*) 'elem_vect_possion: SET ivar1,ivar2 (1:3)'
         read(*,*) ivar1,ivar2
@@ -289,69 +245,66 @@
         go to 100
       endif
   200 continue
-
+#endif
 !
 !---------------------------------------------------------------------
 !    B O U N D A R Y    I N T E G R A L S                            |
 !---------------------------------------------------------------------
 !
-!  ...loop through element faces
-      do ifc=1,nrf
+!..loop through element faces
+   do ifc=1,nrf
 !
-!  .....sign factor to determine the OUTWARD normal unit vector
-        nsign = nsign_param(etype,ifc)
+!  ...sign factor to determine the OUTWARD normal unit vector
+      nsign = nsign_param(etype,ifc)
 !
-!  .....face type
-        ftype = face_type(etype,ifc)
+!  ...face type
+      ftype = face_type(etype,ifc)
 !
-!  .....face order of approximation
-        call face_order(etype,ifc,norder, norderf)
+!  ...face order of approximation
+      call face_order(etype,ifc,norder, norderf)
 !
-!  .....set 2D quadrature
-        call set_2D_int(ftype,norderf,norient_face(ifc), nint,tloc,wtloc)
+!  ...set 2D quadrature
+      call set_2D_int(ftype,norderf,norient_face(ifc), nint,tloc,wtloc)
 !
-!  .....loop through integration points
-        do l=1,nint
+!  ...loop through integration points
+      do l=1,nint
 !
-!  .......face coordinates
-          t(1:2) = tloc(1:2,l)
+!     ...face coordinates
+         t(1:2) = tloc(1:2,l)
 !
-!  .......face parametrization
-          call face_param(etype,ifc,t, xi,dxidt)
+!     ...face parametrization
+         call face_param(etype,ifc,t, xi,dxidt)
 !
-!  .......determine element H1 shape functions
-          call shape3DH(etype,xi,norder,norient_edge,norient_face, &
-                        nrdofH,shapH,gradH)
+!     ...determine element H1 shape functions
+         call shape3DH(etype,xi,norder,norient_edge,norient_face, &
+                       nrdofH,shapH,gradH)
 !
-!  .......geometry
-          call bgeom3D(Mdle,xi,xnod,shapH,gradH,nrdofH,dxidt,nsign, &
-                       x,dxdxi,dxidx,rjac,dxdt,rn,bjac)
+!     ...geometry
+         call bgeom3D(Mdle,xi,xnod,shapH,gradH,nrdofH,dxidt,nsign, &
+                      x,dxdxi,dxidx,rjac,dxdt,rn,bjac)
 !
-!  .......get the surface load (for a single eqn)
-          call getg(Mdle,x,rn, gval)
+!     ...get the surface load (for a single eqn)
+         call getg(Mdle,x,rn, gval)
 !
-!  .......integration weight
-          weight = bjac*wtloc(l)
+!     ...integration weight
+         weight = bjac*wtloc(l)
 !
-!  .......loop through H1 test functions
-          do k1=1,NrdofH
+!     ...loop through H1 test functions
+         do k1=1,NrdofH
             q = shapH(k1)
             do ivar=1,3
-              n1 = (k1-1)*3+ivar
+               n1 = (k1-1)*3+ivar
 !
-!  ...........check if BC flag for the Neumann BC
-              if (ibc(ifc,ivar).eq.2) then
-                Zbloc(n1) =  Zbloc(n1) + gval*q*weight
-              endif
+!           ...check if BC flag for the Neumann BC
+               if (ibc(ifc,ivar).eq.2) then
+                  Zbloc(n1) =  Zbloc(n1) + gval*q*weight
+               endif
             enddo
-          enddo         
-!  .....end loop through integration points
-        enddo
-!
-!  ...end loop through element faces
+         enddo
+!  ...end loop through integration points
       enddo
 !
+!..end loop through element faces
+   enddo
 !
-      end subroutine elem_vect_poisson
-
-
+end subroutine elem_vect_poisson
