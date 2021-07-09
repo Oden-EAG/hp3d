a90f95751871775b030958872784ec88adbf2273
stefanhenneking <stefan.henneking@gmail.com>
2021-07-09 14:40:23 -0500

    Copying the original shape functions package for future reference.

---
+++ trunk/src/element/shape_1_cpy/list_cpy.mk
@@ -0,0 +1,16 @@
+ContExactSequence.F
+Ancillary.F
+Polynomials.F
+BlendProject.F
+Orient.F
+Hexahedron.F
+AffineCoordinates.F
+Tetrahedron.F
+Prism.F
+Triangle.F
+Quadrilateral.F
+Segment.F
+TraceSequence.F
+Pyramid.F
+CheckOrder.F
+0
