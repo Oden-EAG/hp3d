0edd81e870492410021e04dcf62c21a474a4025b
Socratis Petrides <petrides1@llnl.gov>
2022-04-29 16:24:34 -0700

    minor

--- trunk/problems/ACOUSTICS/ULTRAWEAK/celem.F90
+++ trunk/problems/ACOUSTICS/ULTRAWEAK/celem.F90
@@ -47,12 +47,11 @@ subroutine celem(Mdle,Idec,Nrdofs,Nrdofm,Nrdofc,Nodm, &
 !
 !--------------------------------------------------------------------------
 !
-   nbcond = (/1,1,0/) ! removes the bubbles
+   ! nbcond = (/1,1,0/) ! removes the bubbles
    ! call encod(nbcond,10,NR_PHYSA, NODES(Mdle)%bcond)
    ! call set_index(NODES(Mdle)%case,NODES(Mdle)%bcond, NODES(Mdle)%index)
 
-   nbcond = 0; nbcond(2:6) = 1
-   call encod(nbcond,2,NRINDEX, NODES(Mdle)%bcond)
+   ! call encod(nbcond,2,NRINDEX, NODES(Mdle)%bcond)
 
 !..redirect to the system routine
    call celem_system(Mdle,Idec, &
@@ -61,7 +60,7 @@ subroutine celem(Mdle,Idec,Nrdofs,Nrdofm,Nrdofc,Nodm, &
                      Bload,Astif)
 !
 !..reset the BC flags back to zero (unknown)
-   NODES(Mdle)%bcond = 0
+   ! NODES(Mdle)%bcond = 0
    ! call set_index(NODES(Mdle)%case,NODES(Mdle)%bcond, NODES(Mdle)%index)
 !
 end subroutine celem
