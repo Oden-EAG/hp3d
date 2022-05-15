755895ff9de73f1647eb013131b9bfb781a3898b
Brendan <keith10@llnl.gov>
2022-05-14 17:09:11 -0700

    can now print menu in main.F90

--- trunk/problems/MPI_HOSE/common/set_environment.F90
+++ trunk/problems/MPI_HOSE/common/set_environment.F90
@@ -14,19 +14,21 @@ subroutine set_environment
   use testvars
   implicit none
 !
+  integer :: nthreads
+!
 ! Variables relevant to src/modules/environment
 !                        option label     // explanation               // default value             // parameter
-  call get_option_string('-file-control'    ,'Control file'              ,'../common/control/control' ,FILE_CONTROL)
-  call get_option_string('-file-geometry'   ,'Geometry file'             ,'../common/geometries/hexa' ,FILE_GEOM   )
+  call get_option_string('-file-control'    ,'Control file'              ,'./control/control'         ,FILE_CONTROL)
+  call get_option_string('-file-geometry'   ,'Geometry file'             ,'./geometries/sheathed_tube_cyl' ,FILE_GEOM   )
   call get_option_string('-file-phys'       ,'Physics file'              ,'./input/physics'           ,FILE_PHYS   )
   call get_option_string('-file-history'    ,'History file'              ,'./input/history'           ,FILE_HISTORY)
   call get_option_string('-file-err'        ,'Error file'                ,'./output/errorlogs/log.txt',FILE_ERR    )
-  call get_option_string('-file-refinement' ,'Refinement files location' ,'../../../files/ref'        ,FILE_REFINE )
-  call get_option_string('-prefix'          ,'Prefix for paraview files' ,'cube_'                     ,PREFIX      )
+  call get_option_string('-file-refinement' ,'Refinement files location' ,'../../files/ref'           ,FILE_REFINE )
 !
 ! Variables relevant to src/modules/paraview
 !                        option label     // explanation                        // default value     // parameter
-  call get_option_string('-file-vis-upscale','Visualization upscale file location','../../../files/vis',FILE_VIS          )
+  call get_option_string('-prefix'          ,'Prefix for paraview files'          ,'hose_'             ,PREFIX      )
+  call get_option_string('-file-vis-upscale','Visualization upscale file location','../../files/vis'   ,FILE_VIS          )
   call get_option_string('-vis-level'       ,'Visualization upscale level (0-3)'  ,'2'                 ,VLEVEL            )
   call get_option_string('-dir-paraview'    ,'Paraview root directory'            ,'./output/figures/' ,PARAVIEW_DIR      )
   call get_option_bool(  '-paraview-geom'   ,'Dump geom at every Paraview call'   ,.TRUE.              ,PARAVIEW_DUMP_GEOM)
@@ -40,9 +42,10 @@ subroutine set_environment
   ! call get_option_int(   '-error-attribute' ,'1)Displacement, 2)Stress, 3)Combined, 4)Custom' ,DISPLACEMENT    ,IERROR_ATTR)
   call get_option_int(   '-norm-trial'      ,'1)L2, 2)Natural, 3)Custom'                      ,IERROR_NATURAL  ,IERROR_PROB)
   call get_option_int(   '-norm-test'       ,'1) Adj Graph, 2)Natural, 3)Custom'              ,MATHEMATICIANS  ,TEST_NORM  )
-!Other typical LOCAL options - ignored for this problem
-!  call get_option_int(   '-solver'          ,'Solver: 1)frontal, 2)MUMPS, 3)UHM'            ,1               ,isolver    )
-!  call get_option_int(   '-adapt-mode'      ,'0)iso, 1)aniso'                               ,0               ,imode      )
+!
+!..OpenMP threading
+  call get_option_int('-nthreads','Number of OpenMP threads',1,nthreads)
+  call omp_set_num_threads(nthreads)
 !
 ! The variable dp defining enrichment in DPG problems - src/modules/parametersDPG
 !                        option label // explanation               // default  // parameter
