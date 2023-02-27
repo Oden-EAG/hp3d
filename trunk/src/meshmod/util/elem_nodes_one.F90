139732c40d825cf2828883994790ae7ac84011d2
stefanhenneking <stefan.henneking@gmail.com>
2023-02-27 17:10:28 -0600

    further optimization of elem_nodes_one.

--- trunk/src/meshmod/util/elem_nodes_one.F90
+++ trunk/src/meshmod/util/elem_nodes_one.F90
@@ -11,21 +11,22 @@
 !
 !> @date Feb 2023
 !----------------------------------------------------------------------------
-subroutine elem_nodes_one( &
-     Nfath,Nodesl_fath,Norientl_fath,Ison, &
-     Nod,Nodesl,Norientl)
+subroutine elem_nodes_one(Nfath,Nodesl_fath,Norientl_fath,Ison, &
+                          Nod,Nodesl,Norientl)
    use element_data
    use data_structure3D
    use refinements
    implicit none
 !
-   integer,                intent(in)  :: Nfath,Ison
-   integer, dimension(27), intent(in)  :: Nodesl_fath,Norientl_fath
-   integer,                intent(out) :: Nod
-   integer, dimension(27), intent(out) :: Nodesl,Norientl
+   integer, intent(in)  :: Nfath,Ison
+   integer, intent(in)  :: Nodesl_fath(27),Norientl_fath(27)
+   integer, intent(out) :: Nod
+   integer, intent(out) :: Nodesl(27),Norientl(27)
 !
-   type(node) :: fath, cur
-   integer, dimension(6) :: kref_face
+   integer :: ntype_fath,ntype_cur,nref_fath
+   integer :: nvert_fath,nedge_fath
+!
+   integer :: kref_face(6)
    integer :: iref, ireff, iref1, iref2, iref3, nort
    integer :: j, jp, nodp, nodpp, is, is1, n_nodes
 !
@@ -48,23 +49,26 @@ subroutine elem_nodes_one( &
 !
 !-----------------------------------------------------------
 !..initialize output
-   Nodesl   = 0
-   Norientl = 0
-   fath     = NODES(Nfath) ! TODO: MAKES COPY OF NODE (use pointer?)
-   Nod      = fath%first_son+Ison-1
-   cur      = NODES(Nod) ! TODO: MAKES COPY OF NODE (use pointer?)
+   Nodesl(1:27) = 0; Norientl(1:27) = 0
+!
+   ntype_fath = NODES(Nfath)%ntype
+   nvert_fath = nvert(ntype_fath)
+   nedge_fath = nedge(ntype_fath)
+   nref_fath  = NODES(Nfath)%ref_kind
+   Nod        = NODES(Nfath)%first_son+Ison-1
+   ntype_cur  = NODES(Nod)%ntype
 !
 !-----------------------------------------------------------
 !..one step down the tree reconstructing connectivities
-   call find_face_ref_flags(fath%ntype,fath%ref_kind, kref_face)
-   call decode_ref(fath%ntype,fath%ref_kind, iref1,iref2,iref3)
+   call find_face_ref_flags(ntype_fath,nref_fath, kref_face)
+   call decode_ref(ntype_fath,nref_fath, iref1,iref2,iref3)
 !
-   n_nodes = nvert(cur%ntype)+nedge(cur%ntype)+nface(cur%ntype)+1
-   do j=1, n_nodes
+   n_nodes = nvert(ntype_cur)+nedge(ntype_cur)+nface(ntype_cur)+1
+   do j=1,n_nodes
 !
-      jp   = npar_ref(fath%ntype,j,Ison,iref1,iref2,iref3)
-      is   = nson_ref(fath%ntype,j,Ison,iref1,iref2,iref3)
-      nort = nort_ref(fath%ntype,j,Ison,iref1,iref2,iref3)
+      jp   = npar_ref(ntype_fath,j,Ison,iref1,iref2,iref3)
+      is   = nson_ref(ntype_fath,j,Ison,iref1,iref2,iref3)
+      nort = nort_ref(ntype_fath,j,Ison,iref1,iref2,iref3)
 !
       if (is.eq.0) then
 !
@@ -74,19 +78,19 @@ subroutine elem_nodes_one( &
 !
       else
          nodp = Nodesl_fath(jp)
-         select case (Type_nod(fath%ntype, jp))
+         select case (Type_nod(ntype_fath,jp))
          case (MEDG)
             call rotate_edge(Norientl_fath(jp),is,nort)
             Nodesl(j) = Son(nodp,is)
          case (MDLT)
             ! local and global
-            iref  = kref_face(jp-nvert(fath%ntype)-nedge(fath%ntype))
+            iref  = kref_face(jp-nvert_fath-nedge_fath)
             ireff = NODES(nodp)%ref_kind
             call rotate_trian(iref,ireff,Norientl_fath(jp),is,nort)
             Nodesl(j) = Son(nodp,is)
          case (MDLQ)
             ! local and global
-            iref  = kref_face(jp-nvert(fath%ntype)-nedge(fath%ntype))
+            iref  = kref_face(jp-nvert_fath-nedge_fath)
             ireff = NODES(nodp)%ref_kind
             call rotate_quad(iref,ireff,Norientl_fath(jp), is,is1,nort)
             Nodesl(j) = Son(nodp,is)
@@ -108,7 +112,7 @@ subroutine elem_nodes_one( &
    if (iprint.eq.1) then
       write(*,7031) Nod
  7031 format('elem_nodes_one: NODES AND ORIENT FOR mdle = ',i6)
-      select case(cur%ntype)
+      select case(ntype_cur)
       case(MDLN)
          write(*,7103) Nodesl(1:15)
          write(*,7103) Norientl(1:15)
@@ -130,4 +134,3 @@ subroutine elem_nodes_one( &
 #endif
 !
 end subroutine elem_nodes_one
-
