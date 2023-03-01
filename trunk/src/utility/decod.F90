ece32451896a88c0c92774fe5e27d4c6301fb30a
stefanhenneking <stefan.henneking@gmail.com>
2023-02-28 23:29:46 -0600

    updating encod/decod utilities to f90.

---
+++ trunk/src/utility/decod.F90
@@ -0,0 +1,84 @@
+!----------------------------------------------------------------------
+!
+!   routine name       - decod
+!
+!----------------------------------------------------------------------
+!
+!   latest revision    - Feb 2023
+!
+!   purpose            - routine decodes an integer into a sequence
+!                        of digits organized from left to right
+!
+!   arguments :
+!     in:
+!               Nick   - a nickname to be decoded
+!               Mod    - system number
+!               N      - number of the digits stored in the nickname
+!     out:
+!               Narray - the decoded digits
+!
+!----------------------------------------------------------------------
+subroutine decod(Nick,Mod,N, Narray)
+!
+   implicit none
+!
+   integer, intent(in)    :: Nick,Mod,N
+   integer, intent(inout) :: Narray(N)
+!
+!  ...local variables
+   integer :: i,nick1,nick2
+!
+   nick1 = Nick
+   do i=1,N-1
+      nick2 = nick1/Mod
+      Narray(N+1-i) = nick1 - nick2*Mod
+      nick1 = nick2
+   enddo
+   Narray(1)=nick1
+!
+end subroutine decod
+!
+!
+!----------------------------------------------------------------------
+!
+!   routine name       - decodLong
+!
+!----------------------------------------------------------------------
+!
+!   latest revision    - Feb 2023
+!
+!   purpose            - routine decodes a LONG integer into a sequence
+!                        of digits organized from left to right
+!
+!   arguments :
+!     in:
+!               Nick   - a nickname to be decoded (LONG integer)
+!               Mod    - system number
+!               N      - number of the digits stored in the nickname
+!     out:
+!               Narray - the decoded digits
+!
+!----------------------------------------------------------------------
+subroutine decodLong(Nick,Mod,N, Narray)
+!
+   implicit none
+!
+   integer(8), intent(in)  :: Nick
+   integer,    intent(in)  :: Mod,N
+   integer,    intent(out) :: Narray(N)
+!
+!..local variables
+   integer    :: i
+   integer(8) :: nick1,nick2,longmod
+!
+!..be careful with integer type - need to convert!
+   longmod = INT(Mod,8)
+   nick1 = Nick
+   do i=1,N
+      nick2 = nick1/longmod
+!  ...be careful with integer type - need to convert!
+      Narray(N+1-i) = INT(nick1 - nick2*longmod,4)
+      nick1 = nick2
+   enddo
+!
+end subroutine decodLong
---
+++ trunk/src/utility/decode_order.F90
@@ -0,0 +1,96 @@
+!-----------------------------------------------------------------------
+!
+!     routine name      - decode_orderb
+!
+!-----------------------------------------------------------------------
+!
+!     latest revision:  - Feb 2023
+!
+!     purpose:          - routine decodes order of approximation for
+!                         a brick middle node
+!
+!     arguments:
+!       in:
+!             Norderb   - the encoded order
+!       out:
+!             Nord1,2,3 - orders in directions 1,2,3
+!
+!-----------------------------------------------------------------------
+subroutine decode_orderb(Norderb, Nord1,Nord2,Nord3)
+!
+   use parameters , only : MAXP
+   implicit none
+!
+   integer, intent(in)  :: Norderb
+   integer, intent(out) :: Nord1,Nord2,Nord3
+   integer :: naux
+!
+!  RECALL: Norderb = (Nord1*(MAXP+1)+Nord2)*(MAXP+1)+Nord3
+!
+   naux  = Norderb/(MAXP+1)
+   Nord3 = Norderb-naux*(MAXP+1)
+   Nord1 = naux/(MAXP+1)
+   Nord2 = naux-Nord1*(MAXP+1)
+!
+#if DEBUG_MODE
+   if (Nord1<1.or.Nord1>MAXP) then
+      write(*,*)'decode_orderb: OUT OF RANGE Nord1 = ',Nord1
+      stop
+   endif
+   if (Nord2<1.or.Nord2>MAXP) then
+      write(*,*)'decode_orderb: OUT OF RANGE Nord2 = ',Nord2
+      stop
+   endif
+   if (Nord3<1.or.Nord3>MAXP) then
+      write(*,*)'decode_orderb: OUT OF RANGE Nord3 = ',Nord3
+      stop
+   endif
+#endif
+!
+end subroutine decode_orderb
+!
+!
+!-----------------------------------------------------------------------
+!
+!     routine name      - decode_orderq
+!
+!-----------------------------------------------------------------------
+!
+!     latest revision:  - Feb 2023
+!
+!     purpose:          - routine decodes order of approximation for
+!                         a quad middle node
+!
+!     arguments:
+!
+!     in:
+!             Norderq   - the encoded order
+!     out:
+!             Nord1,2   - orders in directions 1,2
+!
+!-----------------------------------------------------------------------
+subroutine decode_orderq(Norderq, Nord1,Nord2)
+!
+   use parameters , only : MAXP
+   implicit none
+!
+   integer, intent(in)  :: Norderq
+   integer, intent(out) :: Nord1,Nord2
+!
+!  RECALL: Norderq = Nord1*(MAXP+1)+Nord2
+!
+   Nord1 = Norderq/(MAXP+1)
+   Nord2 = Norderq-Nord1*(MAXP+1)
+!
+#if DEBUG_MODE
+   if (Nord1<1.or.Nord1>MAXP) then
+      write(*,*)'decode_orderq: OUT OF RANGE Nord1 = ',Nord1
+      stop
+   endif
+   if (Nord2<1.or.Nord2>MAXP) then
+      write(*,*)'decode_orderq: OUT OF RANGE Nord2 = ',Nord2
+      stop
+   endif
+#endif
+!
+end subroutine decode_orderq
