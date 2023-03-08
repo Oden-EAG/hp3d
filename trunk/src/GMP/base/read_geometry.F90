b783edaf9005368d1fc381e85473add8f3668d75
stefanhenneking <stefan.henneking@gmail.com>
2023-03-08 12:39:34 -0600

    minor

--- trunk/src/GMP/base/read_geometry.F90
+++ trunk/src/GMP/base/read_geometry.F90
@@ -8,22 +8,22 @@ subroutine read_geometry(Fp)
    character(len=*),intent(in) :: Fp
 !
    select case(INPUT_FILE)
-!      case(DUMPIN_LEGACY_) ; call dumpin_GMP_LEGACY('files/dumpGMP')
-!         write(*,*)'dumping in GMP (LEGACY)...'
-!         write(*,*)''
+!     case(DUMPIN_LEGACY_) ; call dumpin_GMP_LEGACY('files/dumpGMP')
+!        write(*,*)'dumping in GMP (LEGACY)...'
+!        write(*,*)''
       case(DUMPIN_)        ; call dumpin_GMP       ('files/dumpGMP')
          write(*,*)'dumping in GMP...'
          write(*,*)''
 !  ...LEGACY: DO NOT USE!
-!      case(LEGACY_)        ; call input_geometry   (Fp)
+!     case(LEGACY_)        ; call input_geometry   (Fp)
       case(DEFAULT_)       ; call input_DEFAULT    (Fp)
 !  ...LEGACY: DO NOT USE!
-!      case(COMPACT_)       ; call input_GMPdata    (Fp)
+!     case(COMPACT_)       ; call input_GMPdata    (Fp)
       case(RECONSTRUCT_)   ; call input_reconstruct(Fp)
       case(NETGEN_)        ; call input_NETGEN     (Fp)
       case default
          write(*,*) 'read_geometry: INPUT_FILE'; stop
-      end select
+   end select
 !
 !
 end subroutine read_geometry
