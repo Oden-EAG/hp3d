0a780f94d78080a84262bbeab992a02b7d0fe2bb
Jacob Badger <jcbadger@utexas.edu>
2023-03-11 20:34:30 -0600

    minor changes

--- trunk/src/meshmod/util/elem_show.F90
+++ trunk/src/meshmod/util/elem_show.F90
@@ -4,8 +4,11 @@
 !!
 !> @date      Feb 2023
 subroutine elem_show_var1(Mdle)
+!
    use data_structure3D
+!
    implicit none
+!
    integer, intent(in) :: Mdle
    integer :: nodesl(27),norientl(27)
    integer :: ntype
@@ -19,16 +22,16 @@ subroutine elem_show_var1(Mdle)
 !
    ntype = NODES(Mdle)%ntype
    select case(ntype)
-   case(MDLB)
+   case(BRIC,MDLB)
       write(*,7106) nodesl(1:27)
       write(*,7106) norientl(1:27)
-   case(MDLN)
+   case(TETR,MDLN)
       write(*,7103) nodesl(1:15)
       write(*,7103) norientl(1:15)
-   case(MDLP)
+   case(PRIS,MDLP)
       write(*,7104) nodesl(1:21)
       write(*,7104) norientl(1:21)
-   case(MDLD)
+   case(PYRA,MDLD)
       write(*,7105) nodesl(1:19)
       write(*,7105) norientl(1:19)
    case default
@@ -36,10 +39,10 @@ subroutine elem_show_var1(Mdle)
       stop 1
    end select
 !
- 7103 format(4i6,2x,6i6,2x,4i6,2x,i6)
- 7104 format(6i6,2x,9i6,2x,2i6,2x,3i6,2x,i6)
- 7105 format(5i6,2x,8i6,2x,i6,2x,4i6,2x,i6)
- 7106 format(8i6,2x,12i6,2x,6i6,2x,i6)
+7103 format(4i8,2x,6i8,2x,4i8,2x,i8)
+7104 format(6i8,2x,9i8,2x,2i8,2x,3i8,2x,i8)
+7105 format(5i8,2x,8i8,2x,i8,2x,4i8,2x,i8)
+7106 format(8i8,2x,12i8,2x,6i8,2x,i8)
 !
 end subroutine elem_show_var1
 !
@@ -54,8 +57,11 @@ end subroutine elem_show_var1
 !!
 !> @date      Feb 2023
 subroutine elem_show_var2(Mdle,Ntype,Nodesl,Norientl)
+!
    use node_types
+!
    implicit none
+!
    integer, intent(in) :: Mdle,Ntype
    integer, intent(in) :: Nodesl(27),Norientl(27)
 !
@@ -77,10 +83,10 @@ subroutine elem_show_var2(Mdle,Ntype,Nodesl,Norientl)
       stop 1
    end select
 !
- 7103 format(4i6,2x,6i6,2x,4i6,2x,i6)
- 7104 format(6i6,2x,9i6,2x,2i6,2x,3i6,2x,i6)
- 7105 format(5i6,2x,8i6,2x,i6,2x,4i6,2x,i6)
- 7106 format(8i6,2x,12i6,2x,6i6,2x,i6)
+7103 format(4i8,2x,6i8,2x,4i8,2x,i8)
+7104 format(6i8,2x,9i8,2x,2i8,2x,3i8,2x,i8)
+7105 format(5i8,2x,8i8,2x,i8,2x,4i8,2x,i8)
+7106 format(8i8,2x,12i8,2x,6i8,2x,i8)
 !
 end subroutine elem_show_var2
 
