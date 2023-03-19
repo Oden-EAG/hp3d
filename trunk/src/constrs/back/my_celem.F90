b3bcaa7cf4134ff9fc7be73d9300457831958262
Stefan Henneking <stefan.henneking@gmail.com>
2023-03-18 19:50:18 -0500

    Remove legacy (#114)
    * removing remark on legacy branch
    
    * removing obsolete tutorial files
    
    * removing src/unused
    
    * removing unused GMP shape functions
    
    * removing legacy files from src/*/back/*
    
    * removing legacy files from src/GMP/*/back/*

--- trunk/src/constrs/back/my_celem.F90
+++
@@ -1,8 +0,0 @@
-      subroutine my_celem(Vec)
-
-      implicit none
-      real*8, dimension(:), pointer, intent(out) :: Vec
-
-      nullify(Vec)
-
-      end subroutine my_celem
--- trunk/tutorial/PROJ/linear.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./proj \
-    ./files/linear/control \
-    ./files/linear/linear \
-    ./files/linear/physics
