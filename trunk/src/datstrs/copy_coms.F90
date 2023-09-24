5ec5097015a4215775e8c1d72ae8ca09701d010e
stefanhenneking <stefan.henneking@gmail.com>
2023-09-24 17:41:38 -0500

    fixing an issue in `copy_coms`

--- trunk/src/datstrs/copy_coms.F90
+++ trunk/src/datstrs/copy_coms.F90
@@ -15,7 +15,7 @@ subroutine copy_coms(Src,Dst)
 !
    integer, intent(in)  :: Src,Dst
 !
-   integer :: nod,ndof,iload,ivar
+   integer :: nod,ndof,nvar
 !
 !-------------------------------------------------------------------------------
 !
@@ -35,8 +35,8 @@ subroutine copy_coms(Src,Dst)
       return
    endif
 !
-!$OMP PARALLEL DO                &
-!$OMP PRIVATE(ndof,iload,ivar)   &
+!$OMP PARALLEL DO          &
+!$OMP PRIVATE(ndof,nvar)   &
 !$OMP SCHEDULE(DYNAMIC)
 !..loop through active nodes
    do nod=1,NRNODS
@@ -45,53 +45,41 @@ subroutine copy_coms(Src,Dst)
 !
 !  ...H1 dof
       if (.not. associated(NODES(nod)%dof%zdofH)) goto 10
+      nvar = ubound(NODES(nod)%dof%zdofH,1)
       ndof = ubound(NODES(nod)%dof%zdofH,2)
-      if (ndof > 0) then
-         do iload=1,NRRHS
-            do ivar=1,NRHVAR*NRRHS
-               NODES(nod)%dof%zdofH(ivar,1:ndof,Dst) = &
-               NODES(nod)%dof%zdofH(ivar,1:ndof,Src)
-            enddo
-         enddo
+      if (nvar>0 .and. ndof>0) then
+         NODES(nod)%dof%zdofH(1:nvar,1:ndof,Dst) = &
+         NODES(nod)%dof%zdofH(1:nvar,1:ndof,Src)
       endif
   10  continue
 !
 !  ...H(curl) dof
       if (.not. associated(NODES(nod)%dof%zdofE)) goto 20
+      nvar = ubound(NODES(nod)%dof%zdofE,1)
       ndof = ubound(NODES(nod)%dof%zdofE,2)
-      if (ndof > 0) then
-         do iload=1,NRRHS
-            do ivar=1,NREVAR
-               NODES(nod)%dof%zdofE(ivar,1:ndof,Dst) = &
-               NODES(nod)%dof%zdofE(ivar,1:ndof,Src)
-            enddo
-         enddo
+      if (nvar>0 .and. ndof>0) then
+         NODES(nod)%dof%zdofE(1:nvar,1:ndof,Dst) = &
+         NODES(nod)%dof%zdofE(1:nvar,1:ndof,Src)
       endif
   20  continue
 !
 !  ...H(div) dof
       if (.not. associated(NODES(nod)%dof%zdofV)) goto 30
+      nvar = ubound(NODES(nod)%dof%zdofV,1)
       ndof = ubound(NODES(nod)%dof%zdofV,2)
-      if (ndof > 0) then
-         do iload=1,NRRHS
-            do ivar=1,NRVVAR
-               NODES(nod)%dof%zdofV(ivar,1:ndof,Dst) = &
-               NODES(nod)%dof%zdofV(ivar,1:ndof,Src)
-            enddo
-         enddo
+      if (nvar>0 .and. ndof>0) then
+         NODES(nod)%dof%zdofV(1:nvar,1:ndof,Dst) = &
+         NODES(nod)%dof%zdofV(1:nvar,1:ndof,Src)
       endif
   30  continue
 !
 !  ...L2 dof
       if (.not. associated(NODES(nod)%dof%zdofQ)) goto 40
+      nvar = ubound(NODES(nod)%dof%zdofQ,1)
       ndof = ubound(NODES(nod)%dof%zdofQ,2)
-      if (ndof > 0) then
-         do iload=1,NRRHS
-            do ivar=1,NRQVAR
-               NODES(nod)%dof%zdofQ(ivar,1:ndof,Dst) = &
-               NODES(nod)%dof%zdofQ(ivar,1:ndof,Src)
-            enddo
-         enddo
+      if (nvar>0 .and. ndof>0) then
+         NODES(nod)%dof%zdofQ(1:nvar,1:ndof,Dst) = &
+         NODES(nod)%dof%zdofQ(1:nvar,1:ndof,Src)
       endif
   40  continue
 !
